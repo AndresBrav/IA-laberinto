e3f384b1d3e882bc46170c90ee9cf695533ad776
Andres Bravo <BravoAndres@gmail.com>
2023-06-06 12:13:33 -0400

    actualizacion6

--- IA proyecto/GraficarCamino.py
+++ IA proyecto/GraficarCamino.py
@@ -23,9 +23,19 @@ def cargarGrafico():
     lopez3(img)
     
 
+    bravo1(img)
     bravo2(img)
     bravo3(img)
-    
+    frias(img)
+    torrez1(img)
+    torrez2(img)
+    juan2(img)
+    juan1(img)
+    pasteur1(img)
+    pasteur2(img)
+    pasteur3(img)
+    pasteur4(img)
+    beni(img)
 
     cv2.imshow('img',img)
     cv2.waitKey(0)
@@ -117,9 +127,93 @@ def bravo3(img):
         img[223,i]=(0,0,255)
         img[224,i]=(0,0,255)
 
-#def bravo1(img):
+def bravo1(img):
+     for i in range(35,45):
+        img[220,i]=(0,0,255) 
+        img[221,i]=(0,0,255)
+        img[222,i]=(0,0,255)
+        img[223,i]=(0,0,255)
+        img[224,i]=(0,0,255)
 
+def frias(img):
+     for i in range(75,85):
+        img[240,i]=(0,0,255) 
+        img[241,i]=(0,0,255)
+        img[242,i]=(0,0,255)
+        img[243,i]=(0,0,255)
+        img[244,i]=(0,0,255)
 
+def torrez1(img):
+     for i in range(55,65):
+        img[228,i]=(0,0,255) 
+        img[229,i]=(0,0,255)
+        img[230,i]=(0,0,255)
+        img[231,i]=(0,0,255)
+        img[232,i]=(0,0,255)
+
+def torrez2(img):
+     for i in range(55,65):
+        img[252,i]=(0,0,255) 
+        img[253,i]=(0,0,255)
+        img[254,i]=(0,0,255)
+        img[255,i]=(0,0,255)
+        img[256,i]=(0,0,255)
+
+def juan2(img):
+     for i in range(95,105):
+        img[252,i]=(0,0,255) 
+        img[253,i]=(0,0,255)
+        img[254,i]=(0,0,255)
+        img[255,i]=(0,0,255)
+        img[256,i]=(0,0,255)
+
+def juan1(img):
+     for i in range(95,105):
+        img[228,i]=(0,0,255) 
+        img[229,i]=(0,0,255)
+        img[230,i]=(0,0,255)
+        img[231,i]=(0,0,255)
+        img[232,i]=(0,0,255)
+
+def pasteur1(img):
+    for i in range(11,23):
+        img[327,i]=(0,0,255) 
+        img[328,i]=(0,0,255)
+        img[329,i]=(0,0,255)
+        img[330,i]=(0,0,255)
+        img[331,i]=(0,0,255)
+
+def pasteur2(img):
+    for i in range(53,63):
+        img[327,i]=(0,0,255) 
+        img[328,i]=(0,0,255)
+        img[329,i]=(0,0,255)
+        img[330,i]=(0,0,255)
+        img[331,i]=(0,0,255)
+
+def pasteur3(img):
+    for i in range(83,93):
+        img[327,i]=(0,0,255) 
+        img[328,i]=(0,0,255)
+        img[329,i]=(0,0,255)
+        img[330,i]=(0,0,255)
+        img[331,i]=(0,0,255)
+
+def pasteur4(img):
+    for i in range(110,123):
+        img[327,i]=(0,0,255) 
+        img[328,i]=(0,0,255)
+        img[329,i]=(0,0,255)
+        img[330,i]=(0,0,255)
+        img[331,i]=(0,0,255)
+
+def beni(img):
+    for i in range(11,23):
+        img[353,i]=(0,0,255) 
+        img[354,i]=(0,0,255)
+        img[355,i]=(0,0,255)
+        img[356,i]=(0,0,255)
+        img[357,i]=(0,0,255)
 
 
 
