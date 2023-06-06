b894979768c060912c405ab2ed84fc31b608fe15
Andres Bravo <BravoAndres@gmail.com>
2023-06-06 08:28:04 -0400

    actualizacion4

--- IA proyecto/GraficarCamino.py
+++ IA proyecto/GraficarCamino.py
@@ -1,7 +1,7 @@
 import cv2
 from Casilla import *
 
-sucre=Casilla('Sucre',1,50,1)
+#sucre=Casilla('Sucre',1,50,1)
 
 def GraficarRecorrido(listaRecorrido):
     lista=[]
@@ -14,6 +14,11 @@ def cargarGrafico():
 
     graficarSucre(img)
     Bascope(img)
+    perez1(img)
+    perez2(img)
+    perez3(img)
+    perez4(img)
+    
 
     cv2.imshow('img',img)
     cv2.waitKey(0)
@@ -33,6 +38,41 @@ def Bascope(img):
         img[i,43]=(0,0,255) 
         img[i,44]=(0,0,255)
         img[i,45]=(0,0,255)
+
+def perez1(img):
+    for i in range(61,73):
+        img[85,i]=(0,0,255) 
+        img[86,i]=(0,0,255)
+        img[87,i]=(0,0,255)
+        img[88,i]=(0,0,255)
+        img[89,i]=(0,0,255)
+
+def perez2(img):
+    for i in range(111,123):
+        img[85,i]=(0,0,255) 
+        img[86,i]=(0,0,255)
+        img[87,i]=(0,0,255)
+        img[88,i]=(0,0,255)
+        img[89,i]=(0,0,255)
+
+def perez3(img):
+    for i in range(171,183):
+        img[85,i]=(0,0,255) 
+        img[86,i]=(0,0,255)
+        img[87,i]=(0,0,255)
+        img[88,i]=(0,0,255)
+        img[89,i]=(0,0,255)
+
+def perez4(img):
+    for i in range(221,233):
+        img[85,i]=(0,0,255) 
+        img[86,i]=(0,0,255)
+        img[87,i]=(0,0,255)
+        img[88,i]=(0,0,255)
+        img[89,i]=(0,0,255)
+
+
+cargarGrafico()
         
 
 
