dfb83469f05d31a9972a9dc40522deded7e7ce2e
Andres Bravo <BravoAndres@gmail.com>
2023-06-06 11:31:27 -0400

    actualizacion5

--- IA proyecto/GraficarCamino.py
+++ IA proyecto/GraficarCamino.py
@@ -10,7 +10,7 @@ def GraficarRecorrido(listaRecorrido):
     cargarGrafico()
 
 def cargarGrafico():
-    img=cv2.imread("IA proyecto/mapaIA1.jpg")
+    img=cv2.imread("IA proyecto/mapaIA5.jpg")
 
     graficarSucre(img)
     Bascope(img)
@@ -18,6 +18,13 @@ def cargarGrafico():
     perez2(img)
     perez3(img)
     perez4(img)
+    lopez1(img)
+    lopez2(img)
+    lopez3(img)
+    
+
+    bravo2(img)
+    bravo3(img)
     
 
     cv2.imshow('img',img)
@@ -26,50 +33,95 @@ def cargarGrafico():
     
 def graficarSucre(img):
     for i in range(11,23):
-        img[45,i]=(0,0,255) 
-        img[46,i]=(0,0,255)
-        img[47,i]=(0,0,255)
-        img[48,i]=(0,0,255)
-        img[49,i]=(0,0,255)
+        img[33,i]=(0,0,255) 
+        img[34,i]=(0,0,255)
+        img[35,i]=(0,0,255)
+        img[36,i]=(0,0,255)
+        img[37,i]=(0,0,255)
 
 def Bascope(img):
-    for i in range(62,75):
-        img[i,42]=(0,0,255) 
-        img[i,43]=(0,0,255) 
-        img[i,44]=(0,0,255)
-        img[i,45]=(0,0,255)
+    for i in range(45,55):
+        img[i,32]=(0,0,255) 
+        img[i,33]=(0,0,255) 
+        img[i,34]=(0,0,255)
+        img[i,35]=(0,0,255)
 
 def perez1(img):
-    for i in range(61,73):
-        img[85,i]=(0,0,255) 
-        img[86,i]=(0,0,255)
-        img[87,i]=(0,0,255)
-        img[88,i]=(0,0,255)
-        img[89,i]=(0,0,255)
+    for i in range(41,53):
+        img[60,i]=(0,0,255) 
+        img[61,i]=(0,0,255)
+        img[62,i]=(0,0,255)
+        img[63,i]=(0,0,255)
+        img[64,i]=(0,0,255)
 
 def perez2(img):
-    for i in range(111,123):
-        img[85,i]=(0,0,255) 
-        img[86,i]=(0,0,255)
-        img[87,i]=(0,0,255)
-        img[88,i]=(0,0,255)
-        img[89,i]=(0,0,255)
+    for i in range(71,83):
+        img[60,i]=(0,0,255) 
+        img[61,i]=(0,0,255)
+        img[62,i]=(0,0,255)
+        img[63,i]=(0,0,255)
+        img[64,i]=(0,0,255)
 
 def perez3(img):
-    for i in range(171,183):
-        img[85,i]=(0,0,255) 
-        img[86,i]=(0,0,255)
-        img[87,i]=(0,0,255)
-        img[88,i]=(0,0,255)
-        img[89,i]=(0,0,255)
+     for i in range(111,123):
+        img[60,i]=(0,0,255) 
+        img[61,i]=(0,0,255)
+        img[62,i]=(0,0,255)
+        img[63,i]=(0,0,255)
+        img[64,i]=(0,0,255)
+   
 
 def perez4(img):
-    for i in range(221,233):
-        img[85,i]=(0,0,255) 
-        img[86,i]=(0,0,255)
-        img[87,i]=(0,0,255)
-        img[88,i]=(0,0,255)
-        img[89,i]=(0,0,255)
+     for i in range(151,163):
+        img[60,i]=(0,0,255) 
+        img[61,i]=(0,0,255)
+        img[62,i]=(0,0,255)
+        img[63,i]=(0,0,255)
+        img[64,i]=(0,0,255)
+
+def lopez1(img):
+    for i in range(32,45):
+        img[i,130]=(0,0,255) 
+        img[i,131]=(0,0,255) 
+        img[i,132]=(0,0,255)
+        img[i,133]=(0,0,255)
+
+def lopez2(img):
+    for i in range(62,75):
+        img[i,130]=(0,0,255) 
+        img[i,131]=(0,0,255) 
+        img[i,132]=(0,0,255)
+        img[i,133]=(0,0,255)
+
+def lopez3(img):
+   for i in range(112,125):
+        img[i,130]=(0,0,255) 
+        img[i,131]=(0,0,255) 
+        img[i,132]=(0,0,255)
+        img[i,133]=(0,0,255)
+
+
+def bravo2(img):
+      for i in range(75,85):
+        img[220,i]=(0,0,255) 
+        img[221,i]=(0,0,255)
+        img[222,i]=(0,0,255)
+        img[223,i]=(0,0,255)
+        img[224,i]=(0,0,255)
+
+def bravo3(img):
+     for i in range(111,123):
+        img[220,i]=(0,0,255) 
+        img[221,i]=(0,0,255)
+        img[222,i]=(0,0,255)
+        img[223,i]=(0,0,255)
+        img[224,i]=(0,0,255)
+
+#def bravo1(img):
+
+
+
+
 
 
 cargarGrafico()
